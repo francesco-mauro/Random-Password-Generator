29eac12e91ab8cc779395ab959d36c7a5dc30c96
francesco-mauro <francesco.mauro180@gmail.com>
2024-11-07 15:49:55 +0100

    feat: restriction to paassword generation and new shuffling

--- script/script.js
+++ script/script.js
@@ -11,39 +11,71 @@ function generaPassword() {
     let usaNumeri = document.getElementById('numeri').checked;
     let usaSimboli = document.getElementById('simboli').checked;
 
-    // Controllo almeno un tipo di carattere selezionato
+    // almeno un tipo di carattere sia selezionato
     if (!usaMaiuscole && !usaMinuscole && !usaNumeri && !usaSimboli) {
         alert('Seleziona almeno un tipo di carattere!');
         return;
     }
 
-    // Creazioneo di array con tutti i caratteri disponibili
+    // Array caratteri obbligatori
+    let caratteriObbligatori = [];
+
+    // Array per tutti i caratteri disponibili
     let caratteriDisponibili = [];
 
+    // Caratteri disponibili e selezioniamo uno per tipo
     if (usaMaiuscole) {
         caratteriDisponibili = caratteriDisponibili.concat(lettereMaiuscole);
+        caratteriObbligatori.push(lettereMaiuscole[Math.floor(Math.random() * lettereMaiuscole.length)]);
     }
 
     if (usaMinuscole) {
         caratteriDisponibili = caratteriDisponibili.concat(lettereMinuscole);
+        caratteriObbligatori.push(lettereMinuscole[Math.floor(Math.random() * lettereMinuscole.length)]);
     }
 
     if (usaNumeri) {
         caratteriDisponibili = caratteriDisponibili.concat(numeri);
+        caratteriObbligatori.push(numeri[Math.floor(Math.random() * numeri.length)]);
     }
 
     if (usaSimboli) {
         caratteriDisponibili = caratteriDisponibili.concat(simboli);
+        caratteriObbligatori.push(simboli[Math.floor(Math.random() * simboli.length)]);
     }
 
-    // password
-    let password = '';
+    // lunghezza sufficiente
+    if (lunghezza < caratteriObbligatori.length) {
+        alert('La lunghezza della password è troppo corta per contenere tutti i tipi di caratteri selezionati.');
+        return;
+    }
 
-    for (let i = 0; i < lunghezza; i++) {
+    // aratteri restanti da generare
+    let caratteriDaGenerare = lunghezza - caratteriObbligatori.length;
+
+    // Array password
+    let passwordArray = [];
+
+    // caratteri obbligatori password
+    passwordArray = passwordArray.concat(caratteriObbligatori);
+
+    for (let i = 0; i < caratteriDaGenerare; i++) {
         let indiceCasuale = Math.floor(Math.random() * caratteriDisponibili.length);
-        password += caratteriDisponibili[indiceCasuale];
+        passwordArray.push(caratteriDisponibili[indiceCasuale]);
     }
 
-    // password generata
+    // Mescoliamo l'array della password
+    passwordArray = shuffleArray(passwordArray);
+
+    let password = passwordArray.join('');
+
     document.getElementById('passwordGenerata').textContent = password;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
